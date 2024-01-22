553edb333c3774d657abdf64bd1d8b24e36ea66c
Sukron <sabbayroad@gmail.com>
2024-01-23 01:22:21 +0700

    Menambahkan favicon

--- temp/abiesoft-templates-main.latte--c02bb52b11.php
+++ temp/abiesoft-templates-main.latte--c02bb52b11.php
@@ -43,54 +43,58 @@ final class Templatec02bb52b11 extends Latte\Runtime\Template
     <link href="/assets/css/media.css" rel="stylesheet" type="text/css">
     <link href="/assets/css/shimmer.css" rel="stylesheet" type="text/css">
     <link href="/assets/lib/icon8/css/line-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
+    <link rel="manifest" href="/assets/img/site.webmanifest">
 ';
-		$this->renderBlock('css', get_defined_vars()) /* line 21 */;
+		$this->renderBlock('css', get_defined_vars()) /* line 25 */;
 		echo '
 </head>
 
 <body>
     <div class=\'art\'><div class=\'gradient\'></div></div>
 ';
-		$this->createTemplate('./theme/components/sidebar.latte', $this->params, 'include')->renderToContentType('html') /* line 27 */;
-		$this->createTemplate('./theme/components/navbar.latte', $this->params, 'include')->renderToContentType('html') /* line 28 */;
+		$this->createTemplate('./theme/components/sidebar.latte', $this->params, 'include')->renderToContentType('html') /* line 31 */;
+		$this->createTemplate('./theme/components/navbar.latte', $this->params, 'include')->renderToContentType('html') /* line 32 */;
 		echo '
     <div class="page">
 ';
-		$this->renderBlock('content', get_defined_vars()) /* line 31 */;
+		$this->renderBlock('content', get_defined_vars()) /* line 35 */;
 		echo '    </div>
 ';
-		$this->createTemplate('./theme/components/footer.latte', $this->params, 'include')->renderToContentType('html') /* line 33 */;
+		$this->createTemplate('./theme/components/footer.latte', $this->params, 'include')->renderToContentType('html') /* line 37 */;
 		echo '
 
     <div id=\'toasthere\'></div>
 
 ';
-		$this->createTemplate('./theme/components/modal.latte', $this->params, 'include')->renderToContentType('html') /* line 38 */;
+		$this->createTemplate('./theme/components/modal.latte', $this->params, 'include')->renderToContentType('html') /* line 42 */;
 		echo '    <script src="/assets/js/jquery-3.7.0.js"></script>
     <script src="/assets/js/style.js"></script>
     <script src="/assets/js/alert.js"></script>
     <script src="/assets/js/validasi.js"></script>
 ';
-		$this->renderBlock('js', get_defined_vars()) /* line 43 */;
+		$this->renderBlock('js', get_defined_vars()) /* line 47 */;
 		echo '</body>
 
 </html>';
 	}
 
 
-	/** {block css} on line 21 */
+	/** {block css} on line 25 */
 	public function blockCss(array $ʟ_args): void
 	{
 	}
 
 
-	/** {block content} on line 31 */
+	/** {block content} on line 35 */
 	public function blockContent(array $ʟ_args): void
 	{
 	}
 
 
-	/** {block js} on line 43 */
+	/** {block js} on line 47 */
 	public function blockJs(array $ʟ_args): void
 	{
 	}
